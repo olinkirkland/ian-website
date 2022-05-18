3a17791163d5237ee8731ea6a00e3f204248395d
Olin Kirkland <me@olinkirk.land>
2022-05-19 01:22:55 +0200

    Add 404, and redirect script

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>
 );
