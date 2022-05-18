a40ae76ac42b984fcb392c15d1d17d823f5f392b
Olin Kirkland <me@olinkirk.land>
2022-05-18 19:15:27 +0200

    Updates

---
+++ precache-manifest.9c5f22089d77df694b12f72149cd4c2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7a569e8629270fd482c3",
+    "url": "/ian-website/static/css/main.742d773f.chunk.css"
+  },
+  {
+    "revision": "7a569e8629270fd482c3",
+    "url": "/ian-website/static/js/main.7a569e86.chunk.js"
+  },
+  {
+    "revision": "ccfaed377a9312a06ca7",
+    "url": "/ian-website/static/js/1.ccfaed37.chunk.js"
+  },
+  {
+    "revision": "c7fc11fc74485970ba8e",
+    "url": "/ian-website/static/js/runtime~main.c7fc11fc.js"
+  },
+  {
+    "revision": "1cd203fe797e0250a7df84d1543a3f87",
+    "url": "/ian-website/index.html"
+  }
+];
