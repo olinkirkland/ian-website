64d2304cb1b98f295262a13dc2eff8e0ad1413b6
Olin Kirkland <me@olinkirk.land>
2022-05-18 22:38:11 +0200

    Contentful blog post link, blog model

--- src/components/pages/Post.jsx
+++ src/components/pages/Post.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+import { createClient } from 'contentful';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import PostModel from '../../models/PostModel';
 
 export default function Post() {
-  return <div>Post</div>;
+  const { id } = useParams();
+
+  const client = createClient({
+    space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
+    accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN
+  });
+
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const query = { content_type: 'blogPost', 'fields.slug': id };
+    client.getEntries(query).then(function(untypedBlogPosts) {
+      try {
+        const blogPost = untypedBlogPosts.items[0];
+        setData(new PostModel(blogPost));
+      } catch (error) {
+        // Redirect to NotFound page
+        console.error('Error:', error);
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <p>Post {id}</p>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
---
+++ src/models/PostModel.js
@@ -0,0 +1,46 @@
+export class ContentType {
+  static PARAGRAPH = 'paragraph';
+  static QUOTE = 'blockquote';
+  static HORIZONTAL_RULE = 'hr';
+  static IMAGE = 'embedded-asset-block';
+}
+
+export default class PostModel {
+  constructor(data) {
+    const fields = data.fields;
+    this.title = fields.title;
+    this.date = fields.date;
+    this.tags = fields.tags; // Array
+    this.image = fields.image
+      ? {
+          url: fields.image.fields.file.url,
+          alt: fields.image.fields.title
+        }
+      : null;
+
+    this.body = fields.body.content.map((entry) => {
+      const type = entry.nodeType;
+      let value = null;
+      switch (entry.nodeType) {
+        case ContentType.PARAGRAPH:
+          value = entry.content[0].value;
+          break;
+        case ContentType.QUOTE:
+          value = entry.content[0].content.value;
+          break;
+        case ContentType.IMAGE:
+          value = entry.data.target.fields.file.url;
+          break;
+        case ContentType.HORIZONTAL_RULE:
+          break;
+        default:
+          break;
+      }
+
+      return {
+        type: entry.nodeType,
+        value: value
+      };
+    });
+  }
+}
