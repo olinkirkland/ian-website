310aabcc9eeb2af718142590c0309997a57ce81d
Olin Kirkland <me@olinkirk.land>
2022-08-27 15:09:47 +0200

    Add intro animation

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,21 +3,17 @@ import React, { useEffect } from 'react';
 export default function Home() {
   useEffect(() => {
     // Get all elements with 'hidden' class
-    const hiddenEls = document.getElementsByClassName('hidden');
-    // Loop through all elements and remove the 'hidden' class every 0.2s
-    for (let i = 0; i < hiddenEls.length; i++) {
-      setTimeout(() => {
-        console.log(hiddenEls[i]);
-        // Remove hidden class from hiddenEls[i]
-        hiddenEls[i].classList.remove('hidden');
-      }, i * 200);
-    }
+    const interval = setInterval(() => {
+      const hiddenEls = document.getElementsByClassName('hidden');
+      if (hiddenEls.length === 0) clearInterval(interval);
+      else hiddenEls[0].classList.remove('hidden');
+    }, 100);
   }, []);
 
   return (
     <section className="home">
-      <div className="text-box hidden">
-        <div className="header">
+      <div className="text-box">
+        <div className="header hidden">
           <img
             id="profile"
             src="assets/profile.png"
@@ -27,37 +23,37 @@ export default function Home() {
             Hi, I'm Ian Kirkland<span className="theme">.</span>
           </h1>
         </div>
-        <h2>
+        <h2 className="hidden">
           Young professional looking for <span className="theme">writing</span>{' '}
           and
           <span className="theme"> publishing</span> opportunities in London,
           UK.
         </h2>
-        <p>
+        <p className="hidden">
           Recent graduate of the University of Edinburgh with a{' '}
           <span className="theme">First Class</span> MA in English Literature
           and Linguistics
         </p>
-        <ul className="skills">
+        <ul className="skills hidden">
           <li>Storytelling</li>
           <li>Researching</li>
           <li>Editing</li>
         </ul>
-        <hr />
-        <p>
+        <hr className="hidden" />
+        <p className="hidden">
           As both a diligent creative and a scholar of the creative process
           itself, I continually pursue new methods to foster expression and
           collaboration with and among others. I have found engagement — on the
           page, through a screen, or in person, is an integral component in the
           social project of teaching, and a skill I have carefully honed.
         </p>
-        <p>
+        <p className="hidden">
           I admire the practice of analysis and instructive critique, greatly
           benefit from it personally, and ensure that whenever I am asked to
           give it myself, both the insight and earnestness of my voice encourage
           growth.
         </p>
-        <ul id="home-links">
+        <ul id="home-links" className="hidden">
           <li>
             <a
               className="button cta"
